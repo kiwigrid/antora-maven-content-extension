8e55849048782b2e2670017728e1d08b9cadda30
Jörg Eichhorn <joerg.eichhorn@kiwigrid.com>
2024-12-12 12:38:25 +0100

    test: add antora 3.1.10 and isolate e2e tests even more

--- test/end2end.spec.mjs
+++ test/end2end.spec.mjs
@@ -1,42 +1,82 @@
-import { expect, use } from 'chai';
+import {expect, use} from 'chai';
 import * as td from 'testdouble';
-import tdChai  from 'testdouble-chai';
-use(tdChai(td));
+import tdChai from 'testdouble-chai';
 import chaiFiles from 'chai-files';
-use(chaiFiles);
-const file = chaiFiles.file;
 import finalhandler from 'finalhandler';
 import http from 'http'
 import serveStatic from 'serve-static';
 
-import { mkdtemp } from 'fs/promises';
-import { join } from 'path';
-import { tmpdir } from 'os';
-import { spawn } from 'child_process'
+import {mkdtemp, writeFile} from 'fs/promises';
+import {join} from 'path';
+import {tmpdir} from 'os';
+import {spawn} from 'child_process'
+import packageJson from '../package.json' with { type: "json" };
 
+use(tdChai(td));
+use(chaiFiles);
+const file = chaiFiles.file;
+
+
+async function observeAndLogStreams(process, name) {
+    await new Promise((resolve, reject) => {
+        process.on('error', reject);
+        process.on('exit', (code, signal) => {
+            if (code === 0) {
+                return resolve();
+            }
+            if (signal) {
+                return reject(new Error(`${name} terminated on ${signal}`))
+            } else {
+                return reject(new Error(`${name} terminated with exit code ${code}`))
+            }
+        });
+        process.stdout.on('data', (data) => {
+            console.log(`${name} stdout: ${data}`);
+        });
+        process.stderr.on('data', (data) => {
+            console.log(`${name} stderr: ${data}`);
+        });
+    });
+}
 
 describe('Maven Content Extension', function () {
 
     const antoraVersions = [
-        ["3.0.1", 'antoracli-301', 'antoragen-301'],
-        ["3.0.2", 'antoracli-302', 'antoragen-302'],
-        ["3.0.3", 'antoracli-303', 'antoragen-303'],
-        ["3.1.0", 'antoracli-310', 'antoragen-310'],
-        ["3.1.1", 'antoracli-311', 'antoragen-311'],
-        ["3.1.2", 'antoracli-312', 'antoragen-312'],
-        ["3.1.3", 'antoracli-313', 'antoragen-313'],
-        ["3.1.4", 'antoracli-314', 'antoragen-314'],
-        ["3.1.5", 'antoracli-315', 'antoragen-315'],
-        ["3.1.6", 'antoracli-316', 'antoragen-316'],
-        ["3.1.7", 'antoracli-317', 'antoragen-317'],
-        ["3.1.8", 'antoracli-318', 'antoragen-318'],
-        ["3.1.9", 'antoracli-319', 'antoragen-319']
+        "3.0.1",
+        "3.0.2",
+        "3.0.3",
+        "3.1.0",
+        "3.1.1",
+        "3.1.2",
+        "3.1.3",
+        "3.1.4",
+        "3.1.5",
+        "3.1.6",
+        "3.1.7",
+        "3.1.8",
+        "3.1.9",
+        "3.1.10",
     ];
 
+    const extensionProjectDir = process.cwd();
+    const packFileName = `kiwigrid-antora-maven-content-${packageJson.version}.tgz`
+
+    let extensionTarball;
     let testTmpDir;
     let cacheDir;
     let siteDir;
     let mavenRepo;
+
+    before(async function () {
+        const tmpDir = await mkdtemp(join(tmpdir(), 'antora-mvn-content-pack'));
+        await observeAndLogStreams(spawn(
+            'npm',
+            ['pack', '--pack-destination', `${tmpDir}`],
+            {
+            }
+        ), `npm pack`);
+        extensionTarball = `${tmpDir}/${packFileName}`;
+    })
 
     beforeEach("create tmp folder and start maven repo", async function () {
         testTmpDir = await mkdtemp(join(tmpdir(), 'antora-mvn-content-tests-'));
@@ -61,45 +101,48 @@ describe('Maven Content Extension', function () {
         });
     });
 
-    antoraVersions.forEach(([name, antoraModule, generatorModule]) => {
-        it(`works with antora ${name}`, async function () {
-            this.timeout(10000)
-
-            const antoraProcess = spawn(
-                `node_modules/${antoraModule}/bin/antora`,
-                 [  '--stacktrace',
+    antoraVersions.forEach((version) => {
+        it(`works with antora ${version}`, async function () {
+            this.timeout(20000)
+            const testPackageJson = new Uint8Array(Buffer. from(`
+            {
+              "name": "@kiwigrid/antora-maven-content-test-project",
+              "version": "1.0.0",
+              "dependencies": {
+                "@antora/cli": "${version}",
+                "@antora/site-generator": "${version}",
+                "@kiwigrid/antora-maven-content": "file:${extensionTarball}"
+              }
+            }
+            `));
+            await writeFile(`${testTmpDir}/package.json`, testPackageJson);
+            await observeAndLogStreams(spawn(
+                'npm',
+                ['install'],
+                {
+                    cwd: testTmpDir,
+                }
+            ), `npm install`);
+            await observeAndLogStreams(spawn(
+                'npx',
+                [
+                    'antora',
+                    '--stacktrace',
                     'generate',
                     `--cache-dir=${cacheDir}`,
                     `--to-dir=${siteDir}`,
-                    `--generator=${generatorModule}`,
-                    'test/resources/antora-playbook.yaml'
-                ]
-            );
-            await new Promise((resolve, reject) => {
-                antoraProcess.on('error', reject);
-                antoraProcess.on('exit', (code, signal) => {
-                    if (code === 0) {
-                        return resolve();
-                    }
-                    if (signal) {
-                        return reject(new Error(`Terminated on ${signal}`))
-                    } else {
-                        return reject(new Error(`Terminated with exit code ${code}`))
-                    }
-                });
-                antoraProcess.stdout.on('data',  (data) => {
-                    console.log(`antora ${name} stdout: ${data}`);
-                });
-                antoraProcess.stderr.on('data',  (data) => {
-                    console.log(`antora ${name} stderr: ${data}`);
-                });
-            });
+                    `${extensionProjectDir}/test/resources/antora-playbook.yaml`
+                ],
+                {
+                    cwd: testTmpDir,
+                }
+            ), `antora ${version}`);
 
             expect(file(join(siteDir, 'index.html'))).to.exist;
             expect(file(join(siteDir, 'test-component', 'index.html'))).to.exist;
             expect(file(join(siteDir, 'test-component', 'index.html')))
                 .to.contain('Hello World from the test component.')
-                .and.to.contain(`Antora ${name}`)
+                .and.to.contain(`Antora ${version}`)
         })
     })
 
